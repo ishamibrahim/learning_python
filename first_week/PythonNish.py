e051e22ae0d4a460d7f6962372ae96878333b7da
nishel <nishel@mangaloreinfotech.in>
2016-02-29 16:14:58 +0530

    Update PythonNish.py

--- first_week/PythonNish.py
+++ first_week/PythonNish.py
@@ -1,20 +1,22 @@
-#!/usr/bin/python
+
+ #!/usr/bin/python
 import MySQLdb
 import csv
 import argparse
-
 import sys
-import party 
-from unresolved_party_type import unresolved_party_type_name
 
-connection = MySQLdb.connect(host='localhost', port=3306, user='codaxtr_user', passwd='c0d@xtr', db='codaxtr')
+
+connection = MySQLdb.connect(host='localhost', port=3350, user='root', passwd='', db='codaxtr')
 
 cursor = connection.cursor ()
-cursor.execute ("select p.id,p.fullname,u.id,u.unresolved_party_type_name from party p inner join unresolved_party_type u on u.id=p.id order by p.id limit 10")
+#cursor.execute ("select * from party")
+#cursor.execute ("select p.id,p.fullname,u.id,u.PartyType from party p inner join PartyType u on u.id=p.id order by p.id limit 10")
+cursor.execute("select p.id,p.fullname,u.id,u.unresolved_party_type_name from party p inner join unresolved_party_type u on u.id=p.id order by p.id limit 10")
 data = cursor.fetchall ()
-print row[0], row[1]
-cursor.close ()
-connection.close()
+
+
+for item in data:
+	print item
 
 def write_to_csv (csv_file, result_tuple):
                 if not result_tuple:
@@ -26,5 +28,3 @@ def write_to_csv (csv_file, result_tuple):
                         csv_writer.writerow(row_tuple)
 
                 return None
-sys.exit()
-
